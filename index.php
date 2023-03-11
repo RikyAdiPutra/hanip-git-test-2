9f0313d7709d8a59f6f3a97b6f836d8901c7b292
RikyAdiPutra <rikyadiputra76@gmail.com>
2023-03-11 21:04:55 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel itaque maxime laudantium quae quis perspiciatis adipisci autem distinctio labore deleniti? Quaerat velit incidunt alias placeat ipsa optio adipisci id sunt.</p>
 </body>
 </html>
